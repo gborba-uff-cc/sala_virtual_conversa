0ccbf970af96ecc2d1e10b42b352441ea18b3885
Gabriel Borba <gborba@id.uff.br>
2021-08-08 17:23:12 -0300

    correcao para usar os novos métodos da classe cliente

--- mainCliente.py
+++ mainCliente.py
@@ -1,14 +1,18 @@
 import socket
 from src.cliente_servidor.cliente import Cliente
+import time
 
+# TODO - interface do usuario
 # TODO - endereco deve ser pego pela interface com o usuario
 servidor_endereco = 'localhost'
 servidor_porta = 5000
 
 c = Cliente()
 c.conectaAoServidor(servidor_endereco, servidor_porta)
-strRetornado = c.fazPedidoRegistro('gabrielborba')
-print(strRetornado)
-strRetornado = c.fazPedidoConsulta('gabrielborba')
-print(strRetornado)
-c.fazPedidoEncerramento()
+retorno = c.realizaPedidoRegistro('gabrielborba')
+print(retorno)
+time.sleep(1)
+retorno = c.realizaPedidoConsulta('gabrielborba')
+print(retorno)
+time.sleep(1)
+c.realizaPedidoEncerramento()
