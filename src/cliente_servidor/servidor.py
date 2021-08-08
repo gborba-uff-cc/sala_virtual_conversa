de548f474c57bd0ab77a15d6b8d738e43054db77
Gabriel Borba <gborba@id.uff.br>
2021-08-08 17:22:13 -0300

    organizando as linhas de código

--- src/cliente_servidor/servidor.py
+++ src/cliente_servidor/servidor.py
@@ -23,6 +23,25 @@ class Servidor():
         self._aceitandoConexoes = True
         self._clientesRegistrados = Mutex({})
 
+    def _processaConexao(self, sConexao: socket.socket, endCliente) -> None:
+        """
+        Função que trata de cada conexão estabeecida com o servidor;
+
+        Essa função é executada para cada conexão em uma thread separada
+        """
+        try:
+            processador = geraMaquinaServidor()
+            processador.executa(
+                maquinaEstados=processador,
+                servidor=self,
+                socketConexao=sConexao,
+                enderecoCliente=endCliente,
+                strMsg=Caixa(''))
+        except Exception as e:
+            raise e
+        finally:
+            sConexao.close()
+
     def comecaServir(self) -> None:
         try:
             self._socket.bind((self._endereco, self._porta))
@@ -45,20 +64,6 @@ class Servidor():
 
         self._socket.close()
 
-    def _processaConexao(self, sConexao: socket.socket, endCliente) -> None:
-        try:
-            processador = geraMaquinaServidor()
-            processador.executa(
-                maquinaEstados=processador,
-                servidor=self,
-                socketConexao=sConexao,
-                enderecoCliente=endCliente,
-                strMsg=Caixa(''))
-        except Exception as e:
-            raise e
-        finally:
-            sConexao.close()
-
     def consultaRegistro(self, nome: str):
         """
         Consulta e retorna uma linha da tabela de registro se tiver
@@ -75,6 +80,7 @@ class Servidor():
         return valRetornado
 
     def cadastraCliente(self, nome, ip, porta):
+        """Tenta incluir uma linha na tabela de clientes registrados"""
         cadastrou = False
         with self._clientesRegistrados.lock:
             if isinstance(self._clientesRegistrados.data, dict):
@@ -91,10 +97,11 @@ class Servidor():
         return cadastrou
 
     def descadastraCliente(self, ip, porta):
+        """Remove uma entrada da tabela de clientes registrados"""
         with self._clientesRegistrados.lock:
             if isinstance(self._clientesRegistrados.data, dict):
                 nome = None
-                for k, v in self._clientesRegistrados.data.items():
+                for _, v in self._clientesRegistrados.data.items():
                     if v.ip == ip and v.porta == str(porta):
                         nome = v.nome
                         break
