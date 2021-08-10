ad75d379f1ee85592508218385b6e977ccad633c
Gabriel Borba <gborba@id.uff.br>
2021-08-09 23:56:25 -0300

    fazendo as funções que montam e enviam as mensagem retornarem a mensagem para que possam ser impressas

--- src/aplicacao/mensagens_aplicacao.py
+++ src/aplicacao/mensagens_aplicacao.py
@@ -24,48 +24,61 @@ def fazPedidoRegistro(sConexao: socket, nomeDesejado: str):
     """Solicita o registro do nome no servidor"""
     tipoCabecalho = MensagensAplicacao.REGISTO
     cabecalho = tipoCabecalho.value.cod + ' ' + tipoCabecalho.value.description
-    corpo = nomeDesejado
+    corpo = '\n' + nomeDesejado
+    mensagemCompleta = cabecalho + corpo
     Transmissao.enviaBytes(
         socket=sConexao,
-        msg=(cabecalho + '\n' + corpo).encode('UTF8'))
+        msg=mensagemCompleta.encode('UTF8'))
+    # para mostrar na interface o que esta sendo enviado e recebido
+    return mensagemCompleta
 
 
 def fazRespostaPedidoRegistro(sConexao, registrou: bool):
     """Responde a solicitação de registro do nome feita pelo cliente"""
     tipoCabecalho = MensagensAplicacao.REGISTO_EXITO if registrou else MensagensAplicacao.REGISTO_FALHA
     cabecalho = tipoCabecalho.value.cod + ' ' + tipoCabecalho.value.description
+    corpo = ''
+    mensagemCompleta = cabecalho + corpo
     Transmissao.enviaBytes(
         socket=sConexao,
-        msg=cabecalho.encode('UTF8'))
+        msg=mensagemCompleta.encode('UTF8'))
+    return mensagemCompleta
 
 
 def fazPedidoConsulta(sConexao: socket, nomeDesejado: str):
     """Solicita a consulta de um nome"""
     tipoCabecalho = MensagensAplicacao.CONSULTA
     cabecalho = tipoCabecalho.value.cod + ' ' + tipoCabecalho.value.description
-    corpo = nomeDesejado
+    corpo = '\n' + nomeDesejado
+    mensagemCompleta = cabecalho + corpo
     Transmissao.enviaBytes(
         socket=sConexao,
-        msg=(cabecalho + '\n' + corpo).encode('UTF8'))
+        msg=mensagemCompleta.encode('UTF8'))
+    return mensagemCompleta
 
 
 def fazRespostaPedidoConsulta(sConexao, encontrou: bool, ip: str = '', porta: str = ''):
     """Responde a solicitação de consulta ao nome feita pelo cliente"""
     tipoCabecalho = MensagensAplicacao.CONSULTA_EXITO if encontrou else MensagensAplicacao.CONSULTA_FALHA
     cabecalho = tipoCabecalho.value.cod + ' ' + tipoCabecalho.value.description
-    corpo = (ip + ' ' + porta) if encontrou else ''
+    corpo = ('\n' + ip + ' ' + porta) if encontrou else ''
+    mensagemCompleta = cabecalho + corpo
     Transmissao.enviaBytes(
         socket=sConexao,
-        msg=(cabecalho + '\n' + corpo).encode('UTF8'))
+        msg=mensagemCompleta.encode('UTF8'))
+    return mensagemCompleta
 
 
 def fazPedidoEncerramento(sConexao: socket):
     """Clinte informa que quer encerrar a conexão com o servidor"""
     tipoCabecalho = MensagensAplicacao.ENCERRAMENTO
     cabecalho = tipoCabecalho.value.cod + ' ' + tipoCabecalho.value.description
+    corpo = ''
+    mensagemCompleta = cabecalho + corpo
     Transmissao.enviaBytes(
         socket=sConexao,
-        msg=cabecalho.encode('UTF8'))
+        msg=mensagemCompleta.encode('UTF8'))
+    return mensagemCompleta
 
 
 def recebePedidoOuResposta(sConexao):
