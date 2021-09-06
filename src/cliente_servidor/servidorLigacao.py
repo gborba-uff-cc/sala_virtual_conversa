5fe3a8345fc21eddbb8f5d752f924407fef9545e
Gabriel Borba <gborba@id.uff.br>
2021-09-06 00:58:59 -0300

    mesclando e renomeando arquivos

--- src/cliente_servidor/servidorLigacao.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-class ServidorLigacao():
-
-    def __init__(self):
-        # NOTE - criando um socket udp
-        # NOTE - AF_INET indica um endereco do tipo internet;
-        # NOTE - SOCK_DGRAMindica que o socket é do tipo UDP;
-        sServidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
